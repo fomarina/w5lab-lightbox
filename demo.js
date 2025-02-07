0d9b460f8ed6f8d2343188c50625a0722d79bfdc
Marina Oliveira <fo.marina@gmail.com>
2025-02-06 20:52:24 -0500

    Enhance jQuery Lightbox for Multiple Images & Responsiveness

--- demo.js
+++ demo.js
@@ -1,30 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    /* Open lightbox on button click */
-    $('.lightbox-toggle img').click(function(){
-        $('.backdrop').animate({'opacity':'.50'}, 300, 'linear').css('display', 'block');
+    /* Open lightbox on image click */
+    $('.lightbox-toggle img').click(function() {
+        var imgSrc = $(this).attr('src'); // Pega a URL da imagem clicada
+        $('.box').html('<div class="close">&times;</div><img src="' + imgSrc + '" alt="">'); // Atualiza o conteúdo do lightbox
+        $('.backdrop').fadeIn();
         $('.box').fadeIn();
-
-        //Check if lightbox has an image
-        if ($('.box').contents('img')) {
-            $('.box').contents().remove('img'); //If true, clear image
-        }
-
-        //Get text content in attribute
-        var $altvalue = $(this).attr('alt'); //or var altvalue = $(this).attr('alt');
-
-        if ($altvalue=="Angkor Wat") {
-            var img = $('#photo:nth-child(1) img').clone(); //Duplicate DOM element
-            $('.box').append(img); //Insert duplicated element in another element
-        }
     });
 
-    /* Click to close lightbox */
-    $('.close, .backdrop').click(function(){
-        $('.backdrop').animate({'opacity':'0'}, 300, 'linear', function(){
-            $('.backdrop').css('display', 'none');
-        });
-        $('.box').fadeOut();
+    /* Close lightbox */
+    $('.backdrop, .box').on('click', '.close, .backdrop', function() {
+        $('.backdrop, .box').fadeOut();
     });
 
 });
